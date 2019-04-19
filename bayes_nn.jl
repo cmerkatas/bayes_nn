87d683ac12d36685a537b1f6ea0bc19e07668a65
Christos <merxri@gmail.com>
2019-04-18 21:32:11 -0400

    Add files via upload

--- bayes_nn.jl
+++ bayes_nn.jl
@@ -6,6 +6,7 @@ normalize_data(x) = (x .- mean(x)) ./ std(x);
 
 # This modification of the unpack function generates a series of vectors
 # given a network shape.
+# Originally taken from turing.ml
 function unpack(θ::AbstractVector, network_shape::AbstractVector)
   index = 1
   weights = []
@@ -24,6 +25,7 @@ end;
 
 # Generate an abstract neural network given a shape,
 # and return a prediction.
+# Originally taken from turing.ml
 function nn_forward(x, θ::AbstractVector, network_shape::AbstractVector)
   weights, biases = unpack(θ, network_shape)
   layers = []
@@ -37,6 +39,7 @@ function nn_forward(x, θ::AbstractVector, network_shape::AbstractVector)
 end;
 
 
+# define log posterior density for classification or regression
 function log_posterior(x, y, θ::AbstractVector, network_shape::AbstractVector, precisions::Array{Float64}, τ)
   if isempty(τ)
     log_like = -sum(Flux.binarycrossentropy.(nn_forward(x',θ,network_shape), y'))
@@ -56,11 +59,13 @@ function log_posterior(x, y, θ::AbstractVector, network_shape::AbstractVector, 
   end
 end;
 
+# gradients of the log posterior density with respect to parameters
 function ∇log_posterior(x, y, θ::AbstractVector, network_shape::AbstractVector, precisions::Array{Float64}, τ)
   temp = Tracker.gradient(() -> log_posterior(x, y, θ, network_shape, precisions, τ), Flux.params(θ))
   return collect(temp[θ])
 end
 
+# function for a single iteration of the Hamiltonian monte carlo algorithm
 function HMC(x, y, current_q, network_shape::AbstractVector, precisions::Array{Float64}, τ, ϵ = .1, L = 10)
   q = current_q
   p = rand(Normal(0, 1), length(q))  # independent standard normal variates
@@ -130,6 +135,7 @@ end
 # This function makes predictions based on network shape.
 # Return the average predicted value across
 # multiple weights.
+# Originally taken from turing.ml
 function nn_predict(x, theta, num, network_shape)
     mean([nn_forward(x', theta[i,:], network_shape) for i in 1:1000:num])
 end;
