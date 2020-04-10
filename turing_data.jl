a1420fbe803f01a5ea018a935d55525fac819ae6
Christos <merxri@gmail.com>
2020-04-11 02:14:16 +0300

    Add files via upload

---
+++ turing_data.jl
@@ -0,0 +1,33 @@
+# Number of points to generate.
+N = 80
+M = round(Int, N / 4)
+Random.seed!(1234)
+
+# Generate artificial data.
+x1s = rand(M) * 4.5; x2s = rand(M) * 4.5;
+xt1s = Array([[x1s[i] + 0.5; x2s[i] + 0.5] for i = 1:M])
+x1s = rand(M) * 4.5; x2s = rand(M) * 4.5;
+append!(xt1s, Array([[x1s[i] - 5; x2s[i] - 5] for i = 1:M]))
+
+x1s = rand(M) * 4.5; x2s = rand(M) * 4.5;
+xt0s = Array([[x1s[i] + 0.5; x2s[i] - 5] for i = 1:M])
+x1s = rand(M) * 4.5; x2s = rand(M) * 4.5;
+append!(xt0s, Array([[x1s[i] - 5; x2s[i] + 0.5] for i = 1:M]))
+
+# Store all the data for later.
+xs = [xt1s; xt0s]
+ts = [ones(2*M); zeros(2*M)]
+
+X = hcat(xs...)
+Y = ts#onehotbatch(ts, 0:1)
+
+# Plot data points.
+function plot_data()
+    x1 = map(e -> e[1], xt1s)
+    y1 = map(e -> e[2], xt1s)
+    x2 = map(e -> e[1], xt0s)
+    y2 = map(e -> e[2], xt0s)
+
+    Plots.scatter(x1,y1, color="red", clim = (0,1))
+    Plots.scatter!(x2, y2, color="blue", clim = (0,1))
+end
