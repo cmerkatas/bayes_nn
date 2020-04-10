556da2ad952e8fd1ed084cf7dc574b5e87a682dd
Christos <merxri@gmail.com>
2020-04-11 02:13:47 +0300

    Add files via upload

--- bayes_nn.jl
+++ bayes_nn.jl
@@ -1,122 +1,111 @@
-### data normalizer
+"""
+Neural network constructor
+"""
+mutable struct network
+    num_layers::Int64
+    sizearr::Array{Int64,1}
+    biases::Array{Array{Float64,1},1}
+    weights::Array{Array{Float64,2},1}
+    transfers::Array{Function,1}
+    function network(sizes, activation_functions)
+        num_layers = length(sizes)
+        sizearr = sizes
+        biases = [randn(y) for y in sizes[2:end]]
+        weights = [randn(y, x) for (x, y) in zip(sizes[1:end-1], sizes[2:end])]
+        transfers = [activation_functions[i] for i in 1:length(activation_functions)]
+        return new(num_layers, sizearr, biases, weights, transfers)
+    end
+end
+
+"""
+Evaluate network on input x
+"""
+function nn_forward(net::network, x) # network always outputs layer is always linear
+    for (w, b, h) in zip(net.weights, net.biases, net.transfers[2:end])
+        x = h.(w*x .+ b)
+    end
+    return x
+end
+
+"""
+Log posterior for binary classification (loglikelihood + logprior)
+Loglikelihood can be obtained from the loss function implemented in Flux.
+"""
+function log_posterior(net::network, x, y, prior_sigma)
+    return sum(Flux.binarycrossentropy.(nn_forward(net,x), y)) +
+                0.5*norm(Flux.params([net.weights, net.biases]))^2 / prior_sigma^2
+end
+
+"""
+Function that implements one iteration of the
+Hamiltonian Monte Carlo algorithm to update the weights.
+Leapfrog acts on matrices and arrays
+"""
+function hmc_bnn!(net::network, X, Y, prior_sigma, step_size, num_steps)
+
+    p_for_biases = [randn(y) for y in net.sizearr[2:end]]
+    p_for_weights = [randn(y, x) for (x, y) in zip(net.sizearr[1:end-1], net.sizearr[2:end])]
+    current_K = 0.5 * norm([p_for_biases, p_for_weights])^2
+
+    current_weights = copy(net.weights)
+    current_biases = copy(net.biases)
+    current_U = log_posterior(net, X, Y, prior_sigma)
+
+    grads = Zygote.gradient(m -> log_posterior(m, X, Y, prior_sigma), net)[1][]
+    # new state according to leapfrog for hamilton dynamics
+    p_for_weights =  p_for_weights .- step_size .* grads.weights / 2.0
+    p_for_biases = p_for_biases .- step_size .* grads.biases / 2.0
+    for jump in 1:1:num_steps-1
+        net.weights = net.weights .+ step_size .* p_for_weights# gradK.(p)
+        net.biases = net.biases .+ step_size .* p_for_biases
+
+        # update gradients to use with new  p
+        grads = Zygote.gradient(m -> log_posterior(m, X, Y, prior_sigma), net)[1][]
+        p_for_weights = p_for_weights .- step_size .* grads.weights
+        p_for_biases = p_for_biases .- step_size .* grads.biases
+    end
+    net.weights = net.weights .+ step_size .* p_for_weights # gradK.(p)
+    net.biases = net.biases .+ step_size .* p_for_biases
+
+    # update gradients to use with new  p
+    grads = Zygote.gradient(m -> log_posterior(m, X, Y, prior_sigma), net)[1][]
+
+    # the second half for the kinetic variables
+    p_for_weights = p_for_weights .- step_size .* grads.weights / 2.0
+    p_for_biases = p_for_biases .- step_size .* grads.biases / 2.0   # the second half for the kinetic variables
+
+    p_for_weights = -p_for_weights
+    p_for_biases = -p_for_biases
+
+    # test wethear we accept the new state or not
+    # Evaluate potential and kinetic energies at start and end of trajectory
+    proposed_U = log_posterior(net, X, Y, prior_sigma)
+    proposed_K = 0.5 * norm([p_for_biases, p_for_weights])^2
+    if  rand(Float64) .< exp(current_U - proposed_U + current_K - proposed_K)
+        nothing
+    else
+        net.weights = current_weights
+        net.biases = current_biases
+    end
+end
+
+"""
+Some helper functions.
+"""
 normalize_data(x) = (x .- mean(x)) ./ std(x);
 
-# This modification of the unpack function generates a series of vectors
-# given a network shape.
-# Originally taken from turing.ml
-function unpack(θ::AbstractVector, network_shape::AbstractVector)
-  index = 1
-  weights = []
-  biases = []
-  for layer in network_shape
-      rows, cols, _ = layer
-      size = rows * cols
-      last_index_w = size + index - 1
-      last_index_b = last_index_w + rows
-      push!(weights, reshape(θ[index:last_index_w], rows, cols))
-      push!(biases, reshape(θ[last_index_w+1:last_index_b], rows))
-      index = last_index_b + 1
-  end
-  return weights, biases
-end;
-
-# Generate an abstract neural network given a shape,
-# and return a prediction.
-# Originally taken from turing.ml
-function nn_forward(x, θ::AbstractVector, network_shape::AbstractVector)
-  weights, biases = unpack(θ, network_shape)
-  layers = []
-  for i in eachindex(network_shape)
-      push!(layers, Dense(weights[i],
-          biases[i],
-          eval(network_shape[i][3])))
-  end
-  nn = Chain(layers...)
-  return nn(x)
-end;
-
-
-# define log posterior density for classification or regression
-function log_posterior(x, y, θ::AbstractVector, network_shape::AbstractVector, precisions::Array{Float64})
-  log_like = -sum(Flux.binarycrossentropy.(nn_forward(x',θ,network_shape), y'))
-  weights, biases = unpack(θ, network_shape)
-  theta = Float64[]
-  for layer in 1:1:size(weights)[1]
-    theta = vcat(theta, sqrt(precisions[layer]) .* weights[layer][:], sqrt(precisions[layer]) .* biases[layer][:])
-  end
-  return -(log_like - 0.5norm(theta,2)^2)
+function bnn_posterior!(net::network, weights, biases)
+    net.weights = weights
+    net.biases = biases
 end
 
-# gradients of the log posterior density with respect to parameters
-function ∇log_posterior(x, y, θ::AbstractVector, network_shape::AbstractVector, precisions::Array{Float64})
-  θ=param(θ)
-  temp = Tracker.gradient(() -> log_posterior(x, y, θ, network_shape, precisions), Flux.params(θ))
-  return collect(temp[θ])
-end
-
-# function for a single iteration of the Hamiltonian monte carlo algorithm
-function HMC(x, y, current_q, network_shape::AbstractVector, precisions::Array{Float64}, ϵ = .1, L = 10)
-  q = current_q
-  p = rand(Normal(0, 1), length(q))  # independent standard normal variates
-  current_p = p
-
-  ## prepare gradients
-  #q = param(q)
-
-  # Make a half step for momentum at the beginning
-  p = p .- ϵ .* ∇log_posterior(x, y, q, network_shape, precisions) ./ 2
-  # Alternate full steps for position and momentum
-  for i in 1:1:L
-    # Make a full step for the position
-    q = q .+ ϵ .* p
-    # Make a full step for the momentum, except at end of trajectory
-    if i != L
-      p = p .- ϵ .* ∇log_posterior(x, y, q, network_shape, precisions)
+function cell2array(Z)
+    m, n = size(Z)
+    ZZ = zeros(m, n)
+    for i in 1:1:m
+        for j in 1:1:n
+            ZZ[i,j] = Z[i,j][1]
+        end
     end
-  end
-
-  # Make a half step for momentum at the end.
-
-  p = p .- ϵ * ∇log_posterior(x, y, q, network_shape, precisions) ./ 2
-  # Negate momentum at end of trajectory to make the proposal symmetric
-  p = -p
-  # Evaluate potential and kinetic energies at start and end of trajectory
-  current_U = log_posterior(x, y, current_q, network_shape, precisions)
-  current_K = norm(current_p,2)^2 ./ 2
-  proposed_U = log_posterior(x, y, q, network_shape, precisions)
-  proposed_K = norm(p,2)^2 ./ 2
-  # Accept or reject the state at end of trajectory, returning either
-  # the position at the end of the trajectory or the initial position
-  if log(rand()) < current_U - proposed_U + current_K - proposed_K
-    current_q = q # accept
-  end
-  return collect(current_q)
+    return ZZ
 end
-
-function update_precisions(x, y, θ::AbstractVector, network_shape::AbstractVector, hyperparams::Array{Float64,2})
-  weights, biases = unpack(θ, network_shape)
-  nlayers = size(weights)[1]
-  precisions = zeros(nlayers)
-  for layer in 1:1:nlayers
-    layer_weights = vcat(weights[layer][:], biases[layer][:])
-    alpha_star = hyperparams[layer, 1] + 0.5length(layer_weights)
-    beta_star = hyperparams[layer, 2] + 0.5norm(layer_weights,2)^2
-    precisions[layer] = rand(Gamma(alpha_star, 1/beta_star))
-  end
-  return precisions
-end
-
-
-# Reorder the sampled weights
-function reorder!(weights::Array{Float64,2})
-  new_order=sortperm(vec(mean(weights,dims=1)))
-  weights[:,new_order]
-end
-
-# This function makes predictions based on network shape.
-# Return the average predicted value across
-# multiple weights.
-# Originally taken from turing.ml
-function nn_predict(x, theta, num, network_shape)
-    mean([nn_forward(x, theta[i,:], network_shape) for i in 1:1000:num])
-end;
